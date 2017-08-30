2ae00b8ff317ac2529b96bbb39405097842d6e36
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-30 09:26:53 +0200

    Doing the Exercice3 with a different way

--- Exercice3/main.js
+++ Exercice3/main.js
@@ -1,29 +1,13 @@
 // Variables //
-var ford = document.getElementById("ford");
-var toyota = document.getElementById("toyota");
-var renault = document.getElementById("renault");
-var opel = document.getElementById("opel");
-var div = document.getElementById("div1");
+var liItems = document.getElementsByTagName("li");
 
 
 // Functions //
-function contentInDiv(){
-    document.getElementById("ford").addEventListener("click", fordInDiv);
-    document.getElementById("toyota").addEventListener("click", toyotaInDiv);
-    document.getElementById("renault").addEventListener("click", renaultInDiv);
-    document.getElementById("opel").addEventListener("click", opelInDiv);
-}
-function fordInDiv(){
-document.getElementById("div1").innerHTML = (ford.innerText);
-}
-function toyotaInDiv(){
-    document.getElementById("div1").innerHTML = (toyota.innerText);
-}
-function renaultInDiv(){
-    document.getElementById("div1").innerHTML = (renault.innerText);
-}
-function opelInDiv(){
-    document.getElementById("div1").innerHTML = (opel.innerText);
-}
+
+for (let i = 0; i < liItems.length; i++){
+    liItems[i].onclick = function(){
+        liItems.getElementById("").addEventListener("click",);
+    }
+};
+
 // Script Part //
-contentInDiv();
