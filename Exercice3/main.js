5cb62b4d482298ac5dc6b5012cb5d3ced6a6ae44
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-30 09:49:05 +0200

    Added content in exercice3, and trying to make it working

--- Exercice3/main.js
+++ Exercice3/main.js
@@ -5,12 +5,11 @@ var liItems = document.getElementsByTagName("li");
 // Functions //
 
 for (let i = 0; i < liItems.length; i++){
-    liItems[i].onclick = function(){
+    liItems[i].click
         document.getElementById("li").addEventListener("click", function(){
             document.getElementById("div1").innerHTML = (this.innerText);
 
     }
         )};
-};
 
 // Script Part //
